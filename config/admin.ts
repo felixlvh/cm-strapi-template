346a99e651f2475ce0ee582fb7363064c36f5126
Upwill Admin <admin@upwill.com>
2026-02-07 13:02:02 +0800

    Initial Strapi 5.35.0 template

---
+++ config/admin.ts
@@ -0,0 +1,25 @@
+export default ({ env }) => ({
+  url: env('ADMIN_URL', '/admin'),
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+    sessions: {
+      cookieName: `strapi_admin_refresh_${env('PROJECT_SUBDOMAIN', 'default')}`,
+      keys: [env('ADMIN_JWT_SECRET'), env('JWT_SECRET')],
+    },
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+  },
+  secrets: {
+    encryptionKey: env('ENCRYPTION_KEY'),
+  },
+  flags: {
+    nps: env.bool('FLAG_NPS', true),
+    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+  },
+});
